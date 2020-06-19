9a4c14b5948115359da7dea1c6342799257aee38
Dong Le <lequangdongg@gmail.com>
2020-06-19 21:38:23 +0700

    Updates

---
+++ precache-manifest.4e5359ba07c0c5667c8eaf5c160a929c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f5fe1929b380cf58c68137b5f8104d",
+    "url": "/testcard/index.html"
+  },
+  {
+    "revision": "325036e0b6c25c360c19",
+    "url": "/testcard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8bde047eaac1f67e941c",
+    "url": "/testcard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "325036e0b6c25c360c19",
+    "url": "/testcard/static/js/2.f37b3d5b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/testcard/static/js/2.f37b3d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bde047eaac1f67e941c",
+    "url": "/testcard/static/js/main.1948569b.chunk.js"
+  },
+  {
+    "revision": "728c4af1f171cdb9d3fe",
+    "url": "/testcard/static/js/runtime-main.46cd7e68.js"
+  },
+  {
+    "revision": "adc1603b595f2736da645b81ec444224",
+    "url": "/testcard/static/media/1.adc1603b.jpg"
+  }
+]);
